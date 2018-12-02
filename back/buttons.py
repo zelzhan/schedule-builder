7dfd9a622d367e6eac5476b23241b87ac7d42930
zelzhan <elzhan.zeinulla@nu.edu.kz>
2018-12-02 20:24:06 +0600

    pull

--- back/buttons.py
+++ back/buttons.py
@@ -2,4 +2,5 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 
 class Button(FlaskForm):
-	link = SubmitField()
+	link = SubmitField('Add Course')
+	data = StringField()
