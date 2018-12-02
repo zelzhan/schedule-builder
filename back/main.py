a87e11be49169ea62ca85ea63629842f4671bda9
rabbit <rabbit@rabbit.rabbit>
2018-12-02 14:59:16 +0600

    Welcome page was done

--- back/main.py
+++ back/main.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
-from flask import Flask, flash, redirect, render_template, request, url_for
+from flask import Flask, flash, redirect, render_template, request, url_for, make_response
+from flask_restful import Resource, Api
 from pprint import pprint as pp
 import json
 
 app = Flask(__name__)
+api = Api(app)
+
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
- 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	return "Hello world"
+	return make_response(open('templates/welcome.html').read())
 
-	
+class Builder(Resource):
+    def get(self):
+        return make_response(open('templates/layout.html').read())
+
+api.add_resource(Builder, '/builder') # Route_1
+
+
 if __name__=='__main__':
     app.run(debug=True)
